65b3fd198f9669b9207e08ff2709e2765f629db1
Ben Heidemann <ben@heidemann.dev>
2025-04-26 20:48:05 +0100

    chore: ensure tests rerun when new fixtures are added

---
+++ build.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
+
+    // Watch the fixtures directory to re-run tests if new fixtures are added
+    println!("cargo:rerun-if-changed=src/test/fixtures");
+}
